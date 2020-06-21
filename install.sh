651c41b6bb56139db85b62c9af21fbfaa72de682
Raudonkepuris <54080687+Raudonkepuris@users.noreply.github.com>
2020-06-21 22:47:22 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+clear
+sudo pip3 install --upgrade pip
+sudo pip3 install beautifulsoup4
+sudo cp animewalp /usr/bin/animewalp
+sudo chmod +x /usr/bin/animewalp
+mkdir /home/$USER/Pictures/anime-wallpapers
+clear
+echo "| |  | |     | | | | (_)                 | |                  | |
+| |__| | ___ | |_| |_ _  ___  ___    __ _| |__   ___  __ _  __| |
+|  __  |/ _ \| __| __| |/ _ \/ __|  / _  | _  \ / _ \/ _ | / _  |
+| |  | | (_) | |_| |_| |  __/\__ \ | (_| | | | |  __/ (_| | (_| |
+|_|  |_|\___/ \__|\__|_|\___||___/  \__,_|_| |_|\___|\__,_|\__,_|
+
+"
+
+echo "Beat covid-19, by beating your meat
+
+Script for finding perfect anime girl for your desktop
+By Raudonkepuris
+
+Begin using by writing
+animewalp.py -n
+(images will PROBABLY be saved to /home/YOUR USERNAME/Pictures/anime-wallpapers)"
